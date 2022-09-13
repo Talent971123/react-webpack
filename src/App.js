504f17a49567127f54228e2eb8e08c5acb93eb10
Talent971123 <cheeyongchang1123@outlook.sg>
2022-09-14 04:37:40 +0900

    feat : added css & style loader

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import React from "react";
 
 class App extends React.Component {
+  state = {
+    count: 0,
+  };
   render() {
-    debugger;
-    return <h1>Hello world1</h1>;
+    const { count } = this.state;
+    return (
+      <div>
+        <h1>Hello world1</h1>
+        <h2 className={count > 10 ? "warning" : null}>Count: {count}</h2>
+        <button
+          onClick={() => this.setState((state) => ({ count: state.count + 1 }))}
+        >
+          +
+        </button>
+        <button
+          onClick={() => this.setState((state) => ({ count: state.count - 1 }))}
+        >
+          -
+        </button>
+      </div>
+    );
   }
 }
 
